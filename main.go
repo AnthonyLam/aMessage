d6f28d15179531f61162eed17908ee3ed74e4687
Anthony Lam <thelamacmdr@gmail.com>
2016-10-28 21:27:23 -0700

    Success with route tests

--- main.go
+++ main.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+    "log"
+    "net/http"
+    "fmt"
+)
+
 func main() {
+
+    mymux := http.NewServeMux()
+    mymux.HandleFunc("/sms/new",pduToString)
+    mymux.HandleFunc("/", func(w http.ResponseWriter,req *http.Request){
+        if req.URL.Path != "/" {
+            http.NotFound(w,req)
+            return
+        }
+        fmt.Println("Root dir")
+    })
+
+    // Configure the server
+    server := &http.Server{
+        Addr: ":8080",
+        Handler: mymux,
+    }
+    log.Fatal(server.ListenAndServe())
+}
+
+func pduToString(w http.ResponseWriter,req *http.Request){
+    fmt.Println("You got a new message, but fuck you")
 }
