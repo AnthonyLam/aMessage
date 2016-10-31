6da1e4a6381ec4fa7b5c7dc6c716768cddb3d0bd
Anthony Lam <thelamacmdr@gmail.com>
2016-10-30 22:47:45 -0700

    Add json schema for SMS messages

--- sms_json.go
+++ sms_json.go
@@ -4,20 +4,27 @@ import (
 	"json"
 )
 
+type MessageType int32
+
 const (
-	MESSAGE_TYPE_ALL    = iota
-	MESSAGE_TYPE_INBOX  = iota
-	MESSAGE_TYPE_SENT   = iota
-	MESSAGE_TYPE_DRAFT  = iota
-	MESSAGE_TYPE_OUTBOX = iota
-	MESSAGE_TYPE_FAILED = iota
-	MESSAGE_TYPE_QUEUED = iota
+	Sms_MESSAGE_TYPE_ALL    MessageType = iota
+	Sms_MESSAGE_TYPE_INBOX  MessageType = iota
+	Sms_MESSAGE_TYPE_SENT   MessageType = iota
+	Sms_MESSAGE_TYPE_DRAFT  MessageType = iota
+	Sms_MESSAGE_TYPE_OUTBOX MessageType = iota
+	Sms_MESSAGE_TYPE_FAILED MessageType = iota
+	Sms_MESSAGE_TYPE_QUEUED MessageType = iota
 )
 
-type Message struct {
-	address       string
-	body          string
-	date_received uint64
-	date_sent     uint64
-	person        uint64 // Phone Number
+type Sms struct {
+	Address      string
+	Body         string
+	DateReceived uint64
+	DateSent     uint64
+	Person       uint64 // Phone Number
+	Type         MessageType
+}
+
+func (m *Sms) String() string {
+	return fmt.Sprintf("Address: %s\nBody: %s\n", m.Address, m.Body)
 }
