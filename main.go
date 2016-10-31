6e60d4f75aa94267b5d96cf0721ae5eeb8963d6f
Darshan Parajuli <dparajul@uci.edu>
2016-10-30 22:14:52 -0700

    initial commit

--- main.go
+++ main.go
@@ -1,31 +1,34 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "fmt"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
+	mymux := http.NewServeMux()
+	mymux.HandleFunc("/sms/new", pduToString)
+	mymux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/" {
+			http.NotFound(w, req)
+			return
+		}
+		fmt.Println("Root dir")
+	})
+	mymux.HandleFunc("/foo", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintf(w, "bar\n")
+	})
 
-    mymux := http.NewServeMux()
-    mymux.HandleFunc("/sms/new",pduToString)
-    mymux.HandleFunc("/", func(w http.ResponseWriter,req *http.Request){
-        if req.URL.Path != "/" {
-            http.NotFound(w,req)
-            return
-        }
-        fmt.Println("Root dir")
-    })
-
-    // Configure the server
-    server := &http.Server{
-        Addr: ":8080",
-        Handler: mymux,
-    }
-    log.Fatal(server.ListenAndServe())
+	// Configure the server
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mymux,
+	}
+	log.Fatal(server.ListenAndServe())
 }
 
-func pduToString(w http.ResponseWriter,req *http.Request){
-    fmt.Println("You got a new message, but fuck you")
+func pduToString(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("You got a new message, but fuck you")
 }
